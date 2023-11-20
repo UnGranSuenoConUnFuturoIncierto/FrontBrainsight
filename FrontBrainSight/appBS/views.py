ec7477b74a7dd09b118d6eba47956a40fd0162a7
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-20 15:59:58 -0500

    cambios

--- FrontBrainSight/appBS/views.py
+++ FrontBrainSight/appBS/views.py
@@ -2,6 +2,7 @@ import glob
 import os
 import time
 import cv2
+import csv
 from django.http import JsonResponse
 from django.shortcuts import render
 import nibabel as nib
@@ -9,9 +10,32 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from django.template.loader import render_to_string
 
+#Logica de Inicio
+
+def cargarPacientes():
+    pacientes= []
+    with open("appBS/static/pacientes.csv", 'r', newline='', encoding='utf-8') as archivo_csv:
+        lector_csv = csv.reader(archivo_csv, delimiter=';')
+        for fila in lector_csv:
+            paciente = {
+                "id": fila[0],
+                "nombre": fila[1],
+                "edad": fila[2],
+                "genero": fila[3],
+                "estatura": fila[4],
+                "sangre": fila[5],
+                "ocupacion": fila[6],
+                "foto": fila[7]
+            }
+            pacientes.append(paciente)
+    return pacientes
 
 def home(request):
-    return render(request, "inicio.html")
+    pacientes = cargarPacientes()
+    pacientes_agrupados = [pacientes[i:i + 2] for i in range(0, len(pacientes), 2)]
+    return render(request, "inicio.html", {"pacientes_agrupados": pacientes_agrupados})
+
+#Logica de Resultado
 
 def limpiar_imagenes_antiguas(directorio):
     for archivo in glob.glob(os.path.join(directorio, 'frame*')):
@@ -47,9 +71,14 @@ def getframe(image, i, path):
     cv2.imwrite(path,image[:,:,i])
 
 def informacion(request):
+
+    pacientes = cargarPacientes()
+
     if request.method == 'POST':
 
+        id_paciente = request.POST.get('id_paciente')
         frame_number = int(request.POST.get('frame-number'))
+
         if frame_number > 149:
             frame_number = 1
 
@@ -66,7 +95,8 @@ def informacion(request):
         temp_image_path3 = f'appBS/static/frames/frameflair_{timestamp}.png'
         temp_image_path4 = f'appBS/static/frames/framemask_{timestamp}.png'
         
-        flair, t1, t1ce, t2, test_mask = getTrainData("001")
+        id_datos = "00" + id_paciente
+        flair, t1, t1ce, t2, test_mask = getTrainData(id_datos)
         getframe(t1, frame_number, temp_image_path1)
         getframe(t2, frame_number, temp_image_path2)
         getframe(flair, frame_number, temp_image_path3)
@@ -80,7 +110,12 @@ def informacion(request):
         }
 
         html_content = render_to_string('actualizar.html', context)
+        paciente = pacientes[int(id_paciente)-1]
 
-        return JsonResponse({'html': html_content})
+        return JsonResponse({"html": html_content, "paciente": paciente})
     
-    return render(request, 'informacion.html')
+    else:
+        id_paciente = request.GET.get('id_paciente')
+        paciente = pacientes[int(id_paciente)-1]
+    
+    return render(request, 'informacion.html', {"paciente": paciente})
