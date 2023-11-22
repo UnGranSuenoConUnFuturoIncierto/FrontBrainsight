33d70021ddae18e43a33f053e0b7defc77d81efe
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-21 20:49:40 -0500

    cambios

--- FrontBrainSight/FrontBrainSight/settings.py
+++ FrontBrainSight/FrontBrainSight/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appBS'
+    'appBS',
+    'corsheaders'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'FrontBrainSight.urls'
