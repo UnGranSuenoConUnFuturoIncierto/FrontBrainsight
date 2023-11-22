33ae368d98484c470877bbb65234e9df19faa84c
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-21 21:11:18 -0500

    cambios

--- FrontBrainSight/FrontBrainSight/settings.py
+++ FrontBrainSight/FrontBrainSight/settings.py
@@ -53,13 +53,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-CSRF_COOKIE_SECURE = False
+#CSRF_COOKIE_SECURE = False
 
 ROOT_URLCONF = 'FrontBrainSight.urls'
 
